8caf9cbbbc76acdba1ee1984605adf3a61b0ce66
eriBloo <19932449+EriBloo@users.noreply.github.com>
2020-09-06 18:40:28 +0200

    Added simple computer enemy and score template

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 const Player = function(symbol) {
-	const _computer = false;
-	const _name = `Player ${symbol}`;
+	let _computer = false;
+	let _name = `Player ${symbol}`;
+	let _isComputerTurn = false;
 	
 	const getName = () => _name;
 	const setName = () => _name = name;
 	const getSymbol = () => symbol;
 	const setComputer = () => _computer = !_computer;
 	const isComputer = () => _computer;
+	const iscomputerTurn = () => _isComputerTurn;
+	const computerTurn = (val) => _isComputerTurn = val;
 
-	return {getName, setName, getSymbol, setComputer, isComputer}
+	return {getName, setName, getSymbol, setComputer, isComputer, iscomputerTurn, computerTurn}
 }
 
 const Tile = function(id) {
@@ -27,6 +30,7 @@ const Board = (function() {
 	let _board = [Tile(0), Tile(1), Tile(2), Tile(3), Tile(4), Tile(5), Tile(6), Tile(7), Tile(8)];
 	let _players = [Player("x"), Player("o")];
 	let _currentPlayer = 0;
+	let _currentTurn = 1;
 	let _score = {"x": 0, "o": 0, "tie": 0};
 
 	const getPlayer = (position) => _players[position];
@@ -34,29 +38,37 @@ const Board = (function() {
 	const getCurrentPlayer = () => _players[_currentPlayer];
 	const getTile = (id) => _board[id];
 	const getBoard = () => _board;
+	const getCurrentTurn = () => _currentTurn;
+	const nextTurn = () => _currentTurn += 1;
 	const resetBoard = () => {
 		_board = [Tile(0), Tile(1), Tile(2), Tile(3), Tile(4), Tile(5), Tile(6), Tile(7), Tile(8)];
-		_players = [];
+		_players = [Player("x"), Player("o")];
+		_currentTurn = 1;
 	}
 
-	return {getPlayer, nextPlayer, getCurrentPlayer, getTile, getBoard, resetBoard}
+	return {getPlayer, nextPlayer, getCurrentPlayer, getTile, getBoard, getCurrentTurn, nextTurn, resetBoard}
 })();
 
 const Controller = (function () {
 	const _tileEvents = (() => {
 		for (let tile of Board.getBoard()) {
 			tile.getTileElement().addEventListener("click",() => {
-				if (!tile.getSymbol()) {
+				if (!tile.getSymbol() && !Board.getPlayer(1).iscomputerTurn()) {
 					tile.setSymbol(Board.getCurrentPlayer().getSymbol());
 					Board.nextPlayer();
-					_drawBoard();
+					Board.nextTurn();
+					_drawBoard([tile]);
+
+					if (Board.getPlayer(1).isComputer() && Board.getCurrentTurn() < 9) {
+						_computerTurn();
+					}
 				}
 			})
 		}
 	})();
 
-	const _drawBoard = () => {
-		for (let tile of Board.getBoard()) {
+	const _drawBoard = (tiles = Board.getBoard()) => {
+		for (let tile of tiles) {
 			if (tile.getTileElement().childElementCount > 0) {
 				tile.getTileElement().removeChild(tile.getTileElement().firstChild);
 			}
@@ -73,5 +85,36 @@ const Controller = (function () {
 				tile.getTileElement().appendChild(icon);
 			}
 		}
+	}
+
+	const _toggleComputer = (() => document.querySelector(".toggle").addEventListener("click",(e) => {
+		if (e.target.classList.contains("fa-toggle-off")) {
+			e.target.classList.remove("fa-toggle-off");
+			e.target.classList.add("fa-toggle-on");
+		}
+		else {
+			e.target.classList.remove("fa-toggle-on");
+			e.target.classList.add("fa-toggle-off");
+		}
+		Board.getPlayer(1).setComputer();
+		
+		if (Board.getCurrentPlayer().getSymbol() === "o" && Board.getCurrentTurn() < 9) {
+			_computerTurn();
+		}
+	}))();
+
+	const _computerTurn = () => {
+		Board.getPlayer(1).computerTurn(true);
+		let computerChoice;
+
+		do {
+			computerChoice = Math.floor(Math.random() * 9);
+		} while (Board.getTile(computerChoice).getSymbol());
+
+		Board.getTile(computerChoice).setSymbol(Board.getCurrentPlayer().getSymbol());
+		Board.nextPlayer();
+		Board.nextTurn();
+		_drawBoard([Board.getTile(computerChoice)]);
+		Board.getPlayer(1).computerTurn(false);
 	}
 })();
